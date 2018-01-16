f22fb5d4ea72400e7abb96b679a1092459dfade1
nagarajbr <nagaraj.bettadapura@gmail.com>
2018-01-16 11:11:47 -0600

    edit-all, update

--- app/controllers/interests_controller.rb
+++ app/controllers/interests_controller.rb
@@ -1,41 +1,62 @@
 class InterestsController < ApplicationController
-  before_action :set_interest, only: [:show, :edit, :update, :destroy]
+  before_action  :interest_params, only: [:new, :create, :show, :edit, :edit_all, :update_all_answers, :update, :destroy]
 
   # GET /interests
   # GET /interests.json
   def index
-    @interests = Interest.all
+    @interest = Interest.all
+    #@interests = Interest.find(1,10)
   end
 
   # GET /interests/1
   # GET /interests/1.json
   def show
+    @interest = Interest.find(params[:id])
   end
 
   def update_all_answers
-    #@asda = Interest.all
-    @interests.update(interest_params)
+    #@interests = Interest.all
+    #@interest.update(interest_params)
+    # Rails.logger.error("bos")
+    # Rails.logger.error("#{interest_params.inspect}")
+
+    # interest_params.key.each do |id|
+      
+    #   @interest = Interest.find(params[:answer])
+    #   @interest.update(interest_params)
+    # end
     
 
     # @interests.each do |interest|
     #   interest.update!(answer: 1)
     # end
-    Rails.logger.debug("#{@interests.inspect}")
-    # (0..81).each do |n|
-    #  new_answer = @answers[n]
-    #  question = Interest.find(n+1)
-    #  question.update!(answer: new_answer)
-    # end
+    
+    (1..82).each do |n|
+      new_answer=''
+     new_answer = params[n.to_s]
+     if new_answer.to_i > 0
+      logger.error("#{new_answer}")
+      Interest.where(:id => n).update_all(:answer => new_answer.to_i)
+    end
+     #@interest = Interest.find(n+1)
+     #@interest.update(answer: new_answer)
+    end
     redirect_to interests_url
   end
 
   # GET /interests/new
   def new
-    @interest = Interest.new
+    @interest = Interest.all
   end
+
+  def edit_all
+    @interests = Interest.all
+  end 
 
   # GET /interests/1/edit
   def edit
+    #@interest = Interest.find(params[:id])
+    #@interest = Interest.update(interest_params)
     respond_to do |format|
       if @interest.update(interest_params)
         format.html { redirect_to @interest, notice: 'Your answers were submitted.' }
@@ -66,6 +87,7 @@ class InterestsController < ApplicationController
   # PATCH/PUT /interests/1
   # PATCH/PUT /interests/1.json
   def update
+    #@interest = Interest.find(params[:id])
     respond_to do |format|
       if @interest.update(interest_params)
         format.html { redirect_to @interest, notice: 'Your answers were submitted.' }
@@ -76,6 +98,7 @@ class InterestsController < ApplicationController
       end
     end
   end
+
 
   # DELETE /interests/1
   # DELETE /interests/1.json
@@ -90,12 +113,14 @@ class InterestsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_interest
-      logger.error("#{params[:id]}")
       @interest = Interest.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def interest_params
-      params.permit(:question, :answer)
+      #logger.error("#{params.inspect}")
+      #params.permit(:question, :answer, :id)
+      params.fetch(:interest, {})
+      logger.error("#{params.inspect}")
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
+  
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #root 'interests#show'
+  #post 'update_all_answers' => 'interests#update_all_answers', as: :update_all_answers
+  post 'update' => 'interests#update', as: :update
+  #ost 'edit' => 'interests#edit', as: :edit
+  match 'interests/all/edit_all' => 'interests#edit_all', :as => :edit_all, :via => :get
+  match 'interests/all' => 'interests#update_all_answers', :as => :update_all_answers, :via => :put
   resources :interests
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'interests#index'
-  post 'update_all_answers' => 'interests#update_all_answers', as: :update_all_answers
-  post 'update' => 'interests#update', as: :update
-
   # resources :interests do
   # 	collection do
   # 		get :edit
